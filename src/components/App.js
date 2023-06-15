8a2450daa90a903ebe05d86c636b51fd187ae669
Trimbak Pakhale <118504736+Trimbak15@users.noreply.github.com>
2023-06-15 11:54:57 +0000

    second-commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,11 @@
 import React, {Component, useState} from "react";
 import '../styles/App.css';
+import Para from "./Para";
 
 const App = () => {
   return (
     <div id="main">
-      <p>"I am learning React. My life is getting better."</p>
+      <Para/>
     </div>
   )
 }
