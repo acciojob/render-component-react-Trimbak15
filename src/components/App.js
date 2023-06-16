7dd9c5512e928d50c91b0d428189ab3c2fc38a63
Trimbak Pakhale <118504736+Trimbak15@users.noreply.github.com>
2023-06-16 08:07:20 +0000

    final try

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,10 @@
 import React, {Component, useState} from "react";
 import '../styles/App.css';
-import Para from "./Para";
 
 const App = () => {
   return (
     <div id="main">
-      <Para/>
+      <p>I am learning React. My life is getting better.</p>
     </div>
   )
 }
