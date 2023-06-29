d86d1c99b53e1b19531918e339a84c8b7bd529e8
Trimbak Pakhale <118504736+Trimbak15@users.noreply.github.com>
2023-06-29 15:02:21 +0000

    final-last

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,12 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import React from "react";
+
 
 const App = () => {
+
   return (
-    <div id="main">
+    <div>
       <p>I am learning React. My life is getting better.</p>
     </div>
   )
 }
-
-
 export default App;
